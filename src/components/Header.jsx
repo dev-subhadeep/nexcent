d05cf1904cea525b488a1fe530520656b04b834d
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-03 13:06:40 +0530

    changed logo image import statement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import NavButtons from "./NavButtons"
 import Nav from "./Nav"
-import logo from "../../public/logo.png"
+import logo from "../../public/Logo.png"
 import Image from "next/image"
 
 const Header = () => {
